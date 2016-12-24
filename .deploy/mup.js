a6a9b24fcd103cfc8bb348372268d3284ef13f45
Daniel Lamando <apt@danopia.net>
2016-12-24 01:50:33 -0800

    Configure deployment; fix app naming

---
+++ .deploy/mup.js
@@ -0,0 +1,35 @@
+module.exports = {
+  servers: {
+    one: {
+      host: 'ec2-54-218-73-71.us-west-2.compute.amazonaws.com',
+      username: 'ubuntu',
+      pem: '/home/daniel/.ssh/stardust.pem',
+    },
+  },
+
+  meteor: {
+    name: 'stardust',
+    path: '..',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'https://withstardust.tech',
+      MONGO_URL: 'mongodb://localhost/meteor'
+    },
+
+    dockerImage: 'abernix/meteord:base',
+    deployCheckWaitTime: 60
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
